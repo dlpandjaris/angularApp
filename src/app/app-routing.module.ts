c307aa675c8381fa7ac34916e6e27dca0d6124ea
Morgan Elise Meadows <morganelise@Morgans-MacBook-Pro-3.local>
2022-11-20 15:34:01 -0600

    working on login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GiftRegistryComponent } from './projects/giftRegistry/components/gift-registry/gift-registry.component';
-import { LoginComponent } from './projects/giftRegistry/components/login/login.component';
-import { SignupComponent } from './projects/giftRegistry/components/signup/signup.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'projects/gift-registry', pathMatch:'full'},
-  {path:'projects/gift-registry', component:GiftRegistryComponent},
-  {path:'projects/gift-registry/login', component:LoginComponent},
-  {path:'projects/gift-registry/signup', component:SignupComponent}
+  {path:'projects/gift-registry', component:GiftRegistryComponent}
 ];
 
 @NgModule({
